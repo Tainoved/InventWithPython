4f2f42e2516b8d255c50eee330911271b1435b60
Tainoved <fizteshka@yandex.ru>
2017-01-21 22:00:48 +0300

    check added

--- 04.Guess_the_Number.py
+++ 04.Guess_the_Number.py
@@ -7,10 +7,21 @@ player_name = input();
 
 print('Well, {0}, let\'s play the "Guess the Number" game'.format(player_name))
 
+secret_number = random.randint(1, 20)
+
 guesses_taken = 0
 
 while guesses_taken < 6:
     print('Take a guess')
+    guess = input()
+    guess = int(guess)
+
+    if secret_number > guess:
+        print("Your guess is too low")
+    elif secret_number < guess:
+        print("Your guess is too hight")
+    else:
+        print("You are right!!!")
 
     guesses_taken += 1
 
